68cd505d4b57a62ca9f913bc91102cb28081899c
Michael Ferger <mferger92@gmail.com>
2016-02-12 12:01:00 -0700

    added a few more tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,16 @@ var code = require('../main.js');
 var expect = require('chai').expect
 
 describe("something", function () {
-  it("Will spit out the right year", function () {
+  it("Returns true for 2016", function () {
     expect(code.leapYear(2016)).to.equal(true)
+  })
+  it("Returns false for 2015", function () {
+    expect(code.leapYear(2015)).to.equal(false)
+  })
+  it("Returns true for 1600", function () {
+    expect(code.leapYear(1600)).to.equal(true)
+  })
+  it("Returns false for 1700", function () {
+    expect(code.leapYear(1700)).to.equal(false)
   })
 });
